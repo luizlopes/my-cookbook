3f63610ab1020664c99481a84618eb9167fe5c04
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-06-27 12:43:55 -0300

    criando pagina para cadastrar nova receita

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,18 @@
 class RecipesController < ApplicationController
+  
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    render :show
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
   end
 end
