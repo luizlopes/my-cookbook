6cf28a53eb8c6a2ab65ee2cc55816786dfcd0795
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-07-09 14:45:14 -0300

    Usuário deve preencher como preparar

--- spec/features/user_edit_recipe_spec.rb
+++ spec/features/user_edit_recipe_spec.rb
@@ -76,4 +76,13 @@ feature 'User edit a recipe' do
     # validando
     expect(page).to have_content('Você deve informar todos os dados da receita')
   end
+
+  scenario 'and must fill Como Preparar' do
+    # navegando
+    fill_in 'Como Preparar', with: ''
+    click_on 'Enviar'
+
+    # validando
+    expect(page).to have_content('Você deve informar todos os dados da receita')
+  end
 end
