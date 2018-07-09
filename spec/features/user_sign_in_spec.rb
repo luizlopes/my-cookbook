aba3af19f7ba36d7bf253a70cabf005425a74af7
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-07-09 20:54:33 -0300

    Usuário se autentica no site: login

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'user sign in' do
+  scenario 'from home page' do
+    user = User.create(email: 'luiz@cookbook.com', password: 'cookbook123') #create(:user) 
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Entrar'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Olá #{user.email}")
+  end
+end
