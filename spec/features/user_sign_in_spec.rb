230b07e6bd6df514da05e17e0e72a54f0053b2e1
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-07-09 21:13:49 -0300

    Usuário se autentica no site: logout

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -13,4 +13,19 @@ feature 'user sign in' do
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Olá #{user.email}")
   end
+
+  scenario 'and logout' do
+    user = User.create(email: 'luiz@cookbook.com', password: 'cookbook123') #create(:user) 
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Entrar'
+
+    click_on 'Sair'
+    
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Entrar")
+  end
 end
