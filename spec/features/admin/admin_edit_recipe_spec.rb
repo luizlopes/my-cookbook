db48e93285ab39f225d856ff6476502ba6dc7ce3
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-08-04 16:04:32 -0300

    admin edita receitas

---
+++ spec/features/admin/admin_edit_recipe_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'Admin edit a recipe' do
+
+  scenario 'sucessfully' do
+    joao = User.create(email: 'joao@user.com', password: '123123', admin: false)
+    admin = User.create(email: 'admin@admin.com', password: '123123', admin: true)
+
+    recipe_type = create(:recipe_type)
+    cuisine = create(:cuisine)
+    macarrao = create(:recipe, recipe_type: recipe_type, cuisine: cuisine, 
+                      user: joao)
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: admin.email
+    fill_in 'Senha', with: admin.password
+    click_on 'Entrar'
+
+    click_on macarrao.title
+    click_on 'Editar'
+    fill_in 'Dificuldade', with: 'média'
+    fill_in 'Tempo de Preparo', with: '60'
+    click_on 'Enviar'
+
+    expect(page).to have_content(macarrao.title)
+    expect(page).to have_content('média')
+    expect(page).to have_content('60')
+  end
+end
--- spec/features/admin/admin_sign_in_spec.rb
+++ spec/features/admin/admin_sign_in_spec.rb
@@ -13,4 +13,18 @@ feature 'Admin sign in' do
     expect(page).to have_content('Administrador')
     expect(page).to_not have_content(admin.email)
   end
+
+  scenario 'and logout' do
+    admin = User.create(email: 'admin@admin.com', password: '123123', admin: true)
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: admin.email
+    fill_in 'Senha', with: admin.password
+    click_on 'Entrar'
+    click_on 'Sair'
+
+    expect(page).to_not have_content('Administrador')
+    expect(page).to have_link('Entrar')
+  end
 end
